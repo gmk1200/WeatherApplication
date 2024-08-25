fcd3ff567635cc71082368d1f0eb0c1bd97318e7
Medarametla Alekhya <medarametla.alekhya@hcltech.com>
2024-08-25 16:11:02 +0530

    Fetched Data from api

--- script.js
+++ script.js
@@ -4,6 +4,40 @@
  let currentLongitude;
  let areaList = {};
 let currentName="";
+let baseUrl = "https://api.open-meteo.com/v1/forecast?"
+let params = "&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+
+renderOnLoad();
+
+async function renderOnLoad(){
+    console.log("loading..");
+   getLocation();
+    setTimeout(async()=>{
+        let url= baseUrl+"latitude="+currentLatitude+"&longitude="+currentLongitude+params;
+        let data=await getResponse(url);
+        await populateData(data);
+
+
+        console.log(currentLatitude);
+
+    },1000)
+
+   
+
+}
+
+async function populateData(data){
+    let location=document.getElementById('location')
+    location.innerHTML="Hyderabad";
+    // document.append(location);
+    let temp=document.getElementById('temparatere');
+    temp.innerHTML=data.current.temperature_2m
+
+    let date= document.getElementById('date');
+    date.innerHTML=data.current.time
+
+
+}
 
 function updateDropdown(operation, val, name) {
 
@@ -46,16 +80,21 @@ function updateCurrentLocation(value){
  async function getResponse(url){
     const response = await fetch(url,{method:'GET'});
     const data= await response.json();
+    console.log(data);
     return data;
  }
 
- function getLocation(){
+ async function getLocation(){
     if("geolocation" in navigator){
-        navigator.geolocation.watchPosition(
+         navigator.geolocation.watchPosition(
             function(position){
                 const lat=position.coords.latitude;
                 const lng=position.coords.longitude;
+                currentLatitude = lat;
+                currentLongitude = lng;
+                return lat+'@'+lng;
                 console.log(`Latitude: ${lat}, longitude: ${lng}`);
+                
             }
         )
     }
